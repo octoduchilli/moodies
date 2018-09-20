e944468099d3011d752b01069e3fb349a4b438dc
octoduchilli <joada@octo.com>
2018-09-20 11:56:04 +0200

    firebase for moodies

--- src/firebase.js
+++ src/firebase.js
@@ -2,24 +2,24 @@ import { initializeApp } from 'firebase'
 
 // TEST
 
-const app = initializeApp({
-  apiKey: 'AIzaSyCoEI1RieubL2z_TIwD4R5FHokpgGMvngs',
-  authDomain: 'app-film-bd1b7.firebaseapp.com',
-  databaseURL: 'https://app-film-bd1b7.firebaseio.com',
-  projectId: 'app-film-bd1b7',
-  storageBucket: 'app-film-bd1b7.appspot.com',
-  messagingSenderId: '796383962142'
-})
+// const app = initializeApp({
+//   apiKey: 'AIzaSyCoEI1RieubL2z_TIwD4R5FHokpgGMvngs',
+//   authDomain: 'app-film-bd1b7.firebaseapp.com',
+//   databaseURL: 'https://app-film-bd1b7.firebaseio.com',
+//   projectId: 'app-film-bd1b7',
+//   storageBucket: 'app-film-bd1b7.appspot.com',
+//   messagingSenderId: '796383962142'
+// })
 
 // MOODIES
 
-// const app = initializeApp({
-//   apiKey: 'AIzaSyDNZiZQTmoc3yd0qaOrGqjvvK9zpv9FebA',
-//   authDomain: 'moodies-fr.firebaseapp.com',
-//   databaseURL: 'https://moodies-fr.firebaseio.com',
-//   projectId: 'moodies-fr',
-//   storageBucket: 'moodies-fr.appspot.com',
-//   messagingSenderId: '297594988637'
-// })
+const app = initializeApp({
+  apiKey: 'AIzaSyDNZiZQTmoc3yd0qaOrGqjvvK9zpv9FebA',
+  authDomain: 'moodies-fr.firebaseapp.com',
+  databaseURL: 'https://moodies-fr.firebaseio.com',
+  projectId: 'moodies-fr',
+  storageBucket: 'moodies-fr.appspot.com',
+  messagingSenderId: '297594988637'
+})
 
 export const db = app.database()
