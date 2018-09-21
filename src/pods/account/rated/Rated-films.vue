9ba0f55f01ec6e5195fe145d9e2e5eea9d370eeb
octoduchilli <joada@octo.com>
2018-09-21 14:59:30 +0200

    Modif router-links & add a remove button

--- src/pods/account/rated/Rated-films.vue
+++ src/pods/account/rated/Rated-films.vue
@@ -2,22 +2,26 @@
   <div class="rated-films column align-center">
     <basic-button class="margin-20" :button="buttons.back"/>
     <h1 class="text-center">Liste de vos votes ({{__rated.list.length}})</h1>
-    <p class="text-center" style="height: 17px">{{fetch ? 'Récupération des votes...' : null}}</p>
+    <p class="text-center" style="height: 17px">{{fetch || __user.fetchFilms ? 'Récupération des votes...' : null}}</p>
     <basic-dropdown class="margin-10" :dropdown="__rated.dropdowns.sort"/>
     <ul class="rated-list basic-list wrap justi-center">
-      <router-link class="rated link wrap justi-center align-center margin-10 padding-10" v-for="item in __rated.list" :key="item.id" :to="`/film/${item.id}`">
-        <div class="poster column justi-center align-center">
+      <li class="rated wrap justi-center align-center margin-10 padding-10" v-for="item in __list" :key="item.id">
+        <div @click="removeRate(item)" class="delete flex align-center justi-center">
+          <img src="/static/img/buttons/garbage-white.png" alt=" ">
+        </div>
+        <router-link class="poster link column justi-center align-center" :to="`/film/${item.id}`">
           <img class="item-image" :src="w500(item.poster_path)" onerror="this.src = '/static/img/buttons/picture-white.png'; this.style.opacity = '.2'">
           <p class="item-name text-center">{{item.title}}</p>
-        </div>
+        </router-link>
         <div class="column">
-          <div class="row align-center">
+          <router-link class="link row align-center" :to="`/film/${item.id}`">
             <basic-rate :rate="item.rate"/>
             <p class="row"><span class="text-right" style="min-width: 20px; margin-right: 5px">{{item.rate.value.mouseover / 2}}</span> / 5</p>
-          </div>
-          <p class="text-center"><span style="color: grey; font-size: 10px; margin-right: 5px">Date du vote : </span>{{item.votedAt | moment("Do MMMM YYYY à H:mm")}}</p>
+          </router-link>
+          <p class="text-center" style="font-size: 10px; color: lightgrey; height: 10px">Changez votre note sur la fiche du film</p>
+          <p style="min-width: 270px" class="text-center"><span style="color: grey; font-size: 10px; margin-right: 5px">Date du vote : </span>{{item.votedAt | moment("Do MMMM YYYY à H:mm")}}</p>
         </div>
-      </router-link>
+      </li>
     </ul>
   </div>
 </template>
@@ -83,16 +87,12 @@ export default {
     },
     __rated () {
       return this.$store.state.account.rated
-    }
-  },
-  watch: {
-    'buttons.back.click' () {
-      this.$router.go(-1)
     },
-    '__rated.dropdowns.sort.choose' () {
-      this.__rated.dropdowns.sort.click = false
-
-      let list = this.__rated.list
+    __window () {
+      return this.$store.state.window
+    },
+    __list () {
+      let list = JSON.parse(JSON.stringify(this.__rated.list))
       let sort = this.__rated.dropdowns.sort.items[this.__rated.dropdowns.sort.choose].value
 
       if (sort === 'star+') {
@@ -112,6 +112,27 @@ export default {
           return a.timestamp - b.timestamp
         })
       }
+
+      return list
+    }
+  },
+  watch: {
+    'buttons.back.click' () {
+      this.$router.go(-1)
+    },
+    '__rated.dropdowns.sort.choose' () {
+      this.__rated.dropdowns.sort.click = false
+    }
+  },
+  methods: {
+    removeRate (rate) {
+      let list = this.__rated.list
+
+      db.ref(`users/${this.__user.uid}/rate/${rate.id}`).set(null)
+
+      let index = list.findIndex(_ => String(_.id) === String(rate.id))
+
+      list.splice(index, 1)
     }
   }
 }
