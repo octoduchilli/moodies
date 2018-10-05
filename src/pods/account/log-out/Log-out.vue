db8c8bd14617bad28be2fef1036281b555e2640e
octoduchilli <joada@octo.com>
2018-10-05 17:45:19 +0200

    feat (account log-out) : router-link /account/my-list/modif

--- src/pods/account/log-out/Log-out.vue
+++ src/pods/account/log-out/Log-out.vue
@@ -4,7 +4,8 @@
       <h1 class="text-center">{{__user.infos.lastname ? `${__user.infos.lastname}, vous êtes connecté !` : 'Vous êtes connecté !'}}</h1>
       <basic-button style="margin-bottom: 30px; width: 200px" :button="logOut.button"/>
       <router-link style="text-shadow: 0 0 5px black; text-decoration: underline" class="link margin-10" to="/account/informations">Mes informations</router-link>
-      <router-link style="text-shadow: 0 0 5px black; text-decoration: underline" class="link margin-10" to="/account/my-list/show">Voir ma liste</router-link>
+      <router-link style="text-shadow: 0 0 5px black; text-decoration: underline" class="link margin-10" to="/account/my-list/show">Voir mes films</router-link>
+      <router-link style="text-shadow: 0 0 5px black; text-decoration: underline" class="link margin-10" to="/account/my-list/modif">Modifier mes listes</router-link>
       <router-link style="text-shadow: 0 0 5px black; text-decoration: underline" class="link margin-10" to="/account/rated">Voir mes votes</router-link>
     </div>
   </div>
